c45c2f0111e85a6d0041ad2552fcbae837a97bb3
Tenkara <rajknakka@gmail.com>
2023-02-11 11:23:28 -0700

    adding ninja templating with npoint.io json

--- 59-upgraded blog/main.py
+++ 59-upgraded blog/main.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
+    blog_url = "https://api.npoint.io/939eb0ecdd4b45d4f0ef"
+    response = requests.get(blog_url)
+    all_posts = response.json()
+    return render_template("index.html", posts=all_posts)
     return render_template("index.html")
 
 @app.route("/about")
@@ -13,6 +18,10 @@ def about():
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
+@app.route("/post")
+def post():
+    return render_template("post.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
