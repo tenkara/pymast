448e40c09f16b1afaefc263b91efa57b100a4976
Tenkara <rajknakka@gmail.com>
2023-02-12 14:54:12 -0700

    flask-bootstrap wtforms template inheritance

--- 61-Flask-WTForms/main.py
+++ 61-Flask-WTForms/main.py
@@ -1,15 +1,17 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, Length
+from flask_bootstrap import Bootstrap
 
 class LoginForm(FlaskForm):
-    email = StringField(label='Email', validators=[DataRequired()])
-    password = PasswordField(label='Password', validators=[DataRequired()])
+    email = StringField(label='Email', validators=[DataRequired(), Email()])
+    password = PasswordField(label='Password', validators=[DataRequired(), Length(min=8)])
     submit = SubmitField(label='Log In') 
 
 app = Flask(__name__)
 app.secret_key = 'mysecretkey'
+Bootstrap(app)
 
 
 @app.route("/")
