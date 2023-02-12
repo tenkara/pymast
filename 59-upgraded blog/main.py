8b228630bcc157df0d66bb2bb4ce59cae7ef7fbd
Tenkara <rajknakka@gmail.com>
2023-02-12 05:26:18 -0700

    completed blog fully working

--- 59-upgraded blog/main.py
+++ 59-upgraded blog/main.py
@@ -3,11 +3,12 @@ import requests
 
 app = Flask(__name__)
 
+blog_url = "https://api.npoint.io/939eb0ecdd4b45d4f0ef"
+response = requests.get(blog_url)
+all_posts = response.json()
+
 @app.route("/")
 def home():
-    blog_url = "https://api.npoint.io/939eb0ecdd4b45d4f0ef"
-    response = requests.get(blog_url)
-    all_posts = response.json()
     return render_template("index.html", posts=all_posts)
     return render_template("index.html")
 
@@ -21,14 +22,9 @@ def contact():
 
 @app.route("/post/<int:index>")
 def post(index):
-    requested_post = None
-    blog_url = "https://api.npoint.io/939eb0ecdd4b45d4f0ef"
-    response = requests.get(blog_url)
-    all_posts = response.json()
-    for post in all_posts:
-        if post["id"] == index:
-            requested_post = post
-            return render_template("post.html", post=requested_post)
+    requested_post = all_posts[index - 1]
+    print(requested_post)
+    return render_template("post.html", post=requested_post, image=requested_post['image_url'])
     
 
 if __name__ == "__main__":
