a9ee0598a5a6fd0f11468b5171bf075d9c536002
Tenkara <rajknakka@gmail.com>
2023-02-11 20:44:38 -0700

    display individual blog posts

--- 59-upgraded blog/main.py
+++ 59-upgraded blog/main.py
@@ -19,9 +19,17 @@ def about():
 def contact():
     return render_template("contact.html")
 
-@app.route("/post")
-def post():
-    return render_template("post.html")
+@app.route("/post/<int:index>")
+def post(index):
+    requested_post = None
+    blog_url = "https://api.npoint.io/939eb0ecdd4b45d4f0ef"
+    response = requests.get(blog_url)
+    all_posts = response.json()
+    for post in all_posts:
+        if post["id"] == index:
+            requested_post = post
+            return render_template("post.html", post=requested_post)
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
