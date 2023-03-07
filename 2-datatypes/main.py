100018b524f7f5251a9f65f72d38b9585109a223
Tenkara <rajknakka@gmail.com>
2023-03-07 04:44:33 -0700

    numbers

--- 2-datatypes/main.py
+++ 2-datatypes/main.py
@@ -18,13 +18,39 @@ True
 False
 
 #Type Error
-num_char = len(input("What is your name?"))
-new_num_char = str(num_char)
-print("Your name has " + new_num_char + " characters.")
+# num_char = len(input("What is your name?"))
+# new_num_char = str(num_char)
+# print("Your name has " + new_num_char + " characters.")
 
 #Type Error
 a = float(123)
 print(type(a))
+
+#Type Error
+print(70 + float("100.5"))
+
+#Type Error
+print(str(70) + str(100))
+
+#Type Error
+print(int("100") + int("100"))
+
+#Type Error
+print(123 + float("456.789"))
+
+#Type Error
+print(str(123) + str(456.789))
+
+#Type Error
+print(123 + int("456"))
+
+#Type Error
+print(str(123) + str(456))
+
+num = input("Enter a two digit number: ")
+print(f"Your number is {num}")
+print(f"Your number is {int(num[0]) + int(num[1])}")
+
 
 
 
