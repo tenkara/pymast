181cdd20b2426da92e5ae7c30239c08d7d686f1c
Tenkara <rajknakka@gmail.com>
2023-03-07 04:36:34 -0700

    data types

---
+++ 2-datatypes/main.py
@@ -0,0 +1,33 @@
+#String
+
+print("Hello"[4])
+
+#Integer
+
+print(123 + 345)
+
+
+#Float
+
+3.14159
+
+#Boolean
+
+
+True
+False
+
+#Type Error
+num_char = len(input("What is your name?"))
+new_num_char = str(num_char)
+print("Your name has " + new_num_char + " characters.")
+
+#Type Error
+a = float(123)
+print(type(a))
+
+
+
+
+
+
