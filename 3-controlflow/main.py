ea3605604eaf3463dfffc610a2782f6720d4d705
Tenkara <rajknakka@gmail.com>
2023-03-08 04:13:31 -0700

    control flow if else

---
+++ 3-controlflow/main.py
@@ -0,0 +1,16 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+if (height >= 120):
+    print("You can ride the rollercoaster!")
+else:
+    print("Sorry, you have to grow taller before you can ride.")
+
+# Path: 3-controlflow\main.py
+print("Enter a number to check if it is odd or even.")
+number = int(input("What is your number? "))
+if (number % 2 == 0):
+    print("This is an even number.")
+else:
+    print("This is an odd number.")
+
+    
