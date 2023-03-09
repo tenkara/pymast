94c3eceb69708d5a07cac3c4cecede40f1744cdc
Tenkara <rajknakka@gmail.com>
2023-03-09 05:16:39 -0700

    more for loops

--- 5-loops/main.py
+++ 5-loops/main.py
@@ -1,22 +1,42 @@
-fruits = ["apple", "banana", "cherry"]
-for fruit in fruits:
-  print(fruit)
-print(fruits)
+# fruits = ["apple", "banana", "cherry"]
+# for fruit in fruits:
+#   print(fruit)
+# print(fruits)
 
-students_height = input("Input a list of student heights ").split()
-for n in range(0, len(students_height)):
-    students_height[n] = int(students_height[n])
-print(students_height)
+# students_height = input("Input a list of student heights ").split()
+# for n in range(0, len(students_height)):
+#     students_height[n] = int(students_height[n])
+# print(students_height)
 
-total_height = 0
-for height in students_height:
-    total_height += height
-print(total_height)
+# total_height = 0
+# for height in students_height:
+#     total_height += height
+# print(total_height)
 
-number = int(input("How many students are there? "))
-total_height = 0
-for student in range(0, number):
-    height = int(input("Enter student height: "))
-    total_height += height 
-print(f"Total height: {total_height}")
-print(f"Average height: {round(total_height / number)}")
+# number = int(input("How many students are there? "))
+# total_height = 0
+# for student in range(0, number):
+#     height = int(input("Enter student height: "))
+#     total_height += height 
+# print(f"Total height: {total_height}")
+# print(f"Average height: {round(total_height / number)}")
+
+# max_number
+# Write your code below this row 👇
+# scores = [78, 65, 89, 86, 55, 91, 64, 89]
+# max_score = 0
+# avg_score = 0
+# number_of_scores = 0
+# for score in scores:
+#         if score > max_score:
+#             max_score = score
+# print(max_score)
+
+# add numbers from 1 to 100
+total = 0
+for number in range(1, 101):
+    if number % 2 == 0:
+        total += number
+print(total)
+
+
