6ed19b5fb39587d76f6bdaf438a5c910deb0a048
Tenkara <rajknakka@gmail.com>
2023-03-11 08:34:50 -0700

    initial commit

---
+++ 7-lists-hangman/challenge2.py
@@ -0,0 +1,22 @@
+import random
+word_list = ["aardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+# testing code
+print(f'Pssst, the solution is {chosen_word}.')
+
+# TODO-1 - create an empty List called display.
+# For each letter in the chosen_word, add a "_" to 'display'.
+display = []
+for letter in chosen_word:
+    display.append("_")
+print(display)
+
+guess = input("Guess a letter: ").lower()    
+
+# TODO-2 - Loop through each position in the chosen_word;
+# if the letter is that position matches the guess then reveal that letter in the display at that position
+for guess in chosen_word:
+    display[chosen_word.index(guess)] = guess
+
+# TODO-3 - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_"
+print(display)
