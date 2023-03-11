dd2ee0060417eae0c4e40751c41a1ed0c6fe2210
Tenkara <rajknakka@gmail.com>
2023-03-11 14:03:16 -0700

    bug fix on shift

--- 8-parameters-functions/main.py
+++ 8-parameters-functions/main.py
@@ -1,18 +1,50 @@
 import math
 
-def prime_checker(number):
-    is_prime = True
-    for i in range(2, number):
-        if number % i == 0:
-            is_prime = False
-            print(f"{number} is divisible by {i}")
-    if is_prime:
-        print("It's a prime number.")
-    else:
-        print("It's not a prime number.")
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-n = int(input("Check this number: "))
-prime_checker(number=n)
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+
+def encrypt(text, shift):
+    cipher_text = ""
+    for letter in text:
+        position = alphabet.index(letter)
+        new_position = position + shift
+        cipher_text += alphabet[new_position]
+    print(f"The encoded text is {cipher_text}")
+
+def decrypt(text, shift):
+    plain_text = ""
+    for letter in text:
+        position = alphabet.index(letter)
+        new_position = position - shift
+        plain_text += alphabet[new_position]
+    print(f"The decoded text is {plain_text}")
+
+if direction == "encode":
+    encrypt(text=text, shift=shift)
+elif direction == "decode":
+    decrypt(text=text, shift=shift)
+else:
+    print("Invalid direction")
+
+
+# def prime_checker(number):
+#     is_prime = True
+#     for i in range(2, number):
+#         if number % i == 0:
+#             is_prime = False
+#             print(f"{number} is divisible by {i}")
+#     if is_prime:
+#         print("It's a prime number.")
+#     else:
+#         print("It's not a prime number.")
+
+# n = int(input("Check this number: "))
+# prime_checker(number=n)
 
 
 # def greet():
