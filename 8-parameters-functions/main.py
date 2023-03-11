1c03ca68b44d29e151a70872782df5f2993bff4d
Tenkara <rajknakka@gmail.com>
2023-03-11 13:46:36 -0700

    prime number checker

--- 8-parameters-functions/main.py
+++ 8-parameters-functions/main.py
@@ -1,37 +1,52 @@
 import math
 
-def greet():
-    print("Hello")
-    print("Good Morning")
-    print("How are you?")
+def prime_checker(number):
+    is_prime = True
+    for i in range(2, number):
+        if number % i == 0:
+            is_prime = False
+            print(f"{number} is divisible by {i}")
+    if is_prime:
+        print("It's a prime number.")
+    else:
+        print("It's not a prime number.")
 
-# Call the function
-greet()
+n = int(input("Check this number: "))
+prime_checker(number=n)
 
-# Compare this function from 6-functions\challenge1.py:
 
-def greet_with_name(name = "Guest"):
-    print(f"Hello {name}")
-    print("Good Morning")
-    print("How are you?")
+# def greet():
+#     print("Hello")
+#     print("Good Morning")
+#     print("How are you?")
 
-# Call the function
-greet_with_name("Raj")
+# # Call the function
+# greet()
 
-def greet_with(name, location):
-    print(f"Hello {name}")
-    print(f"What is it like in {location}?")
+# # Compare this function from 6-functions\challenge1.py:
 
-# Call the function
-greet_with("Raj", "India")
-greet_with(location = "India", name = "Raj")
+# def greet_with_name(name = "Guest"):
+#     print(f"Hello {name}")
+#     print("Good Morning")
+#     print("How are you?")
 
-def paint_calc(height, width, cover):
-    number_of_cans = math.ceil((height * width) / cover)
-    print(f"You'll need {number_of_cans} cans of paint.")
+# # Call the function
+# greet_with_name("Raj")
 
-# Call the function
-test_h = int(input("Height of wall: "))
-test_w = int(input("Width of wall: "))
-coverage = 5
-paint_calc(height = test_h, width = test_w, cover = coverage)
+# def greet_with(name, location):
+#     print(f"Hello {name}")
+#     print(f"What is it like in {location}?")
+
+# # Call the function
+# greet_with("Raj", "India")
+# greet_with(location = "India", name = "Raj")
+
+# def paint_calc(height, width, cover):
+#     number_of_cans = math.ceil((height * width) / cover)
+#     print(f"You'll need {number_of_cans} cans of paint.")
+
+# # Call the function
+# test_h = int(input("Height of wall: "))
+# test_w = int(input("Width of wall: "))
+# coverage = 5
+# paint_calc(height = test_h, width = test_w, cover = coverage)
