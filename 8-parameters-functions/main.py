54a6b6f8cfcf83d5335a6a00484a007d3f08e9de
Tenkara <rajknakka@gmail.com>
2023-03-11 14:22:51 -0700

    initial caeser

--- 8-parameters-functions/main.py
+++ 8-parameters-functions/main.py
@@ -2,34 +2,49 @@ import math
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
+
+
+def caeser(start_text, shift_amount, cipher_direction):
+    end_text = ""
+    if cipher_direction == "decode":
+        shift_amount *= -1
+    for char in start_text:
+        position = alphabet.index(char)
+        new_position = position + shift_amount
+        end_text += alphabet[new_position]
+    print(f"Here's the {cipher_direction}d result: {end_text}")
+
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
+caeser(start_text=text, shift_amount=shift, cipher_direction=direction)
 
 #TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
 
-def encrypt(text, shift):
-    cipher_text = ""
-    for letter in text:
-        position = alphabet.index(letter)
-        new_position = position + shift
-        cipher_text += alphabet[new_position]
-    print(f"The encoded text is {cipher_text}")
+# def encrypt(text, shift):
+#     cipher_text = ""
+#     for letter in text:
+#         position = alphabet.index(letter)
+#         new_position = position + shift
+#         cipher_text += alphabet[new_position]
+#     print(f"The encoded text is {cipher_text}")
 
-def decrypt(text, shift):
-    plain_text = ""
-    for letter in text:
-        position = alphabet.index(letter)
-        new_position = position - shift
-        plain_text += alphabet[new_position]
-    print(f"The decoded text is {plain_text}")
+# def decrypt(text, shift):
+#     plain_text = ""
+#     for letter in text:
+#         position = alphabet.index(letter)
+#         new_position = position - shift
+#         print(f"Old position: {position}, new position: {new_position}")
+#         print(f"Old letter: {letter}, new letter: {alphabet[new_position]}")
+#         plain_text += alphabet[new_position]
+#     print(f"The decoded text is {plain_text}")
 
-if direction == "encode":
-    encrypt(text=text, shift=shift)
-elif direction == "decode":
-    decrypt(text=text, shift=shift)
-else:
-    print("Invalid direction")
+# if direction == "encode":
+#     encrypt(text=text, shift=shift)
+# elif direction == "decode":
+#     decrypt(text=text, shift=shift)
+# else:
+#     print("Invalid direction")
 
 
 # def prime_checker(number):
