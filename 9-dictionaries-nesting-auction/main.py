089dbc0c148769c0ac06bb12962971e027cb6064
Tenkara <rajknakka@gmail.com>
2023-03-11 20:44:40 -0700

    auction complete

--- 9-dictionaries-nesting-auction/main.py
+++ 9-dictionaries-nesting-auction/main.py
@@ -52,3 +52,13 @@ travel_log = [
         "total_visits": 5,
     },
 ]
+
+def add_new_country(country_visited, times_visited, cities_visited):
+    new_country = {}
+    new_country["country"] = country_visited
+    new_country["total_visits"] = times_visited
+    new_country["cities_visited"] = cities_visited
+    travel_log.append(new_country)
+    
+add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
+print(travel_log)
