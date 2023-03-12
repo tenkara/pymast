8729c59e6c72ec43189d289db3a82c6c03791b9e
Tenkara <rajknakka@gmail.com>
2023-03-12 16:19:07 -0600

    coffee machine oop

--- 16-oop/main.py
+++ 16-oop/main.py
@@ -1,20 +1,31 @@
-# import another_module
-# print(another_module.another_variable)
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
 
-# from turtle import Turtle, Screen
-# timmy = Turtle()
-# print (timmy)
-# timmy.shape("turtle")
-# timmy.color("coral")
-# timmy.forward(100)
 
-# my_screen = Screen()
-# print(my_screen.canvheight)
+# set up the objects
+my_coffee_maker = CoffeeMaker()
+my_money_machine = MoneyMachine()
+menu = Menu()
 
-# my_screen.exitonclick()
+is_on = True
 
-from prettytable import PrettyTable
-table = PrettyTable()
-table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
-table.add_column("Type", ["Electric", "Water", "Fire"])
-print(table)
+# TODO 6. Loop
+while is_on:
+    # TODO 6.1. Ask user by printing “What would you like? (espresso/latte/cappuccino):”
+    options = menu.get_items()
+    user_input = input(f"What would you like? ({options}): ")
+
+    if user_input == "off":
+        is_on = False
+    elif user_input == "report":
+        my_coffee_maker.report()
+        my_money_machine.report()
+    else:
+        my_drink = menu.find_drink(user_input)
+        print(my_drink)
+        if my_coffee_maker.is_resource_sufficient(my_drink) and (my_money_machine.make_payment(my_drink.cost)):
+                my_coffee_maker.make_coffee(my_drink)
+
+
+
