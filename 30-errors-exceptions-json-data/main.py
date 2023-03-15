2bf3abc339effa97497fb3f92c8aac8988238835
Tenkara <rajknakka@gmail.com>
2023-03-14 20:16:11 -0600

    initial commit

---
+++ 30-errors-exceptions-json-data/main.py
@@ -0,0 +1,12 @@
+#file not found error
+#try except block
+try:
+    with open("a_file.txt") as file:
+        file.read()
+    a_dictionary = {"key": "value"}
+    print(a_dictionary["key"])
+except FileNotFoundError:
+    file = open("a_file.txt", "w")
+    file.write("Something")
+except KeyError as error_message:
+    print(f"The key {error_message} does not exist.")
