a3ded8460fcb8b6a35fab18760b6083bfeebfc09
Tenkara <rajknakka@gmail.com>
2023-03-16 06:00:40 -0600

    lat long

--- 33-API-ISS/main.py
+++ 33-API-ISS/main.py
@@ -1,4 +1,8 @@
 import requests
 response =requests.get(url="http://api.open-notify.org/iss-now.json")
-if 
-print(response.status_code)
+response.raise_for_status()
+
+longitude = response.json()['iss_position']['longitude']
+latitude = response.json()['iss_position']['latitude']
+iss_position = (latitude, longitude)
+print(iss_position)
