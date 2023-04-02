804ed94b9ba9e0c2b4ee7f3239fd1b7a5eaf9d23
Tenkara <rajknakka@gmail.com>
2023-04-02 16:04:24 -0600

    working nutrionix sheety

--- 38-workout-openai/main.py
+++ 38-workout-openai/main.py
@@ -10,4 +10,32 @@ headers = {
     "x-remote-user-id": "0",
 }
 
+exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
 
+# query = input("Tell me which exercises you did: ")
+
+# exercise_params = {
+#     "query": query,
+    
+# }
+
+# response = requests.post(url=exercise_endpoint, json=exercise_params, headers=headers)
+# result = response.json()
+# print(result)
+
+get_workouts = requests.get(os.getenv("SHEETY_ENDPOINT"))
+result = get_workouts.json()
+print(result)
+
+
+add_workout = requests.post(os.getenv("SHEETY_ENDPOINT"), json={
+    "workout": {
+        "date": "02/05/2021",
+        "time": "12:00",
+        "exercise": "Cycling",
+        "duration": "45",
+        "calories": "500",
+    }
+})
+
+print(add_workout.text)
